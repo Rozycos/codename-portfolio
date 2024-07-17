e4aeb748141aad2108533f4cc6792726b71aec9d
rozycos <rozycos@gmail.com>
2024-07-18 00:14:49 +0200

    create pages structure

--- app/example/page.js
+++ app/example/page.js
@@ -1,3 +1,5 @@
+import '../../app/globals.scss';
+
 export default function Example() {
     return (
       <main>
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import './globals.scss';
+
+export default function Home() {
+  return (
+    <main>
+        <h1>HELLOWORD PORTFOLIO ROZYCOS aaaaaaa</h1>
+    </main>
+  );
+}
